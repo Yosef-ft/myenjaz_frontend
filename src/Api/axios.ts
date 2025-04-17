c0fe0ea46825b9e5e9f85124fe62d0de79ef064b
Yosef <yosefetene02@gmail.com>
2025-04-17 03:44:05 +0300

    Added interceptor to included token

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,10 +1,22 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  // baseURL: "https://myenjaz-backend.onrender.com/api/",
-  // baseURL: "http://localhost:5500/api/",
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,  
+  timeout: 10000,
 });
+
+// Add request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
